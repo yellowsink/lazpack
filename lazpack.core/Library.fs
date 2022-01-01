3f8d8bac9fd2410ce8535fbabf60ea594957a096
Cain Atkinson <yellowsink@protonmail.com>
2022-01-01 00:40:10 +0000

    some code

--- lazpack.core/Library.fs
+++ lazpack.core/Library.fs
@@ -1,5 +1,21 @@
 ﻿namespace lazpack.core
 
-module Say =
-    let hello name =
-        printfn $"Hello %s{name}"
+open System.Net
+open System.Net.Http
+
+type ruleset = string
+
+type repo = { url: string; rulesets: ruleset list }
+
+module Fetcher =
+    let fetch (url: string) =
+        async {
+            let client = new HttpClient()
+            let! resp = client.GetAsync url |> Async.AwaitTask
+
+            return
+                (if resp.StatusCode = HttpStatusCode.OK then
+                     resp.Content.ToString()
+                 else
+                     "")
+        }
